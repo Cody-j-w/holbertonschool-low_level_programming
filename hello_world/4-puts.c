cee979c6341e2691cad64e92255cfbf32beab63d
Cody <codywalenciak@gmail.com>
2023-09-20 11:30:38 -0700

    C function that prints a line with puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint of program
+ * Description: a program that writes out a line of text using puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
