9ac06a48240383348a6af5474ddb204f56336ec9
Cody <codywalenciak@gmail.com>
2023-09-20 11:45:46 -0700

    C function that prints a line with printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint of program
+ * Description: a program that writes out a line of text using printf
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
