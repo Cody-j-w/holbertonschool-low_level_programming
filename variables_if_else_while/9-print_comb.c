ed1d8fdc1ec0430d440d44988dfdb621696b81fe
Cody <codywalenciak@gmail.com>
2023-09-21 11:50:54 -0700

    overthought the problem a little bit, went a bit overkill. Toned down 9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,29 +2,24 @@
 
 /**
  * main - entrypoint of program
- * Description: writes out the single-digit base 10 numbers
+ * Description: writes out digits 0 through 9, separated by a comma and space
  * Return: 0 (always success)
  */
 
 int main(void)
 {
 	int alpha;
-	int beta;
 
 	for (alpha = 0 ; alpha <= 9 ; alpha++)
 	{
-		for (beta = 0 ; beta <= 9 ; beta++)
+		if (alpha > 0)
 		{
-			if (alpha > 0 || beta > 0)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			if (alpha > 0)
-				putchar('0' + alpha);
-			putchar('0' + beta);
+			putchar(',');
+			putchar(' ');
 		}
+		putchar('0' + alpha);
 	}
+	putchar('\n');
 
 	return (0);
 }
