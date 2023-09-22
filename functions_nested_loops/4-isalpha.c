fee059df8f5103450968004847fa27801a9de6d7
Cody <codywalenciak@gmail.com>
2023-09-22 09:56:10 -0700

    added _isalpha function

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -5,6 +5,7 @@
  *
  * Description: Uses an if statement to compare parameter c against ASCII
  * values and determine whether or not it is a letter.
+ * Return: a boolean - true if c is a letter, false if not.
  */
 
 int _isalpha(int c)
