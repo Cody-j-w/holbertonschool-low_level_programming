3fb93a5955bf7160a64d741a86f0a5a23ba30cf4
Cody <codywalenciak@gmail.com>
2023-09-22 08:39:06 -0700

    alphabet function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * main - entrypoint of program
+ *
+ * Description: prints alphabet
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
