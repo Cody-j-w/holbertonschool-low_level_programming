37ba45da167ca4240191da26aed0babde070b6a8
Cody <codywalenciak@gmail.com>
2023-09-22 10:57:57 -0700

    print_sign function

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints a sign to signify whether a given integer is positive,
  *  negative, or zero.
- * @n - an integer, could be positive, negative, or zero.
+ * @n: an integer, could be positive, negative, or zero.
  *
  * Description: compares n, finds its value, and prints + for a positive value
  * - for a negative value, or 0 for a value of zero.
