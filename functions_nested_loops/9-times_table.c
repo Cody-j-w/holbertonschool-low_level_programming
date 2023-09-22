ec5b46385c34534b2293642b7439fd1a0e1d6b4d
Cody <codywalenciak@gmail.com>
2023-09-22 14:21:13 -0700

    making Betty happy.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_table - print the 9 times table, starting from 0
  *
- * Description: use a nested loop, division, and a modulus to print 
+ * Description: use a nested loop, division, and a modulus to print
  * a table of 2 digit integers using _putchar
  */
 
@@ -25,22 +25,22 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (sum < 10 )
+				if (sum < 10)
 					_putchar(' ');
 			}
-			
+
 			if (sum > 9)
 			{
-				_putchar((sum/10)+'0');
-				_putchar((sum%10)+'0');
+				_putchar((sum / 10) + '0');
+				_putchar((sum % 10) + '0');
 			}
 			else
 			{
 				_putchar(sum + '0');
 			}
-			
+
 		}
 		_putchar('\n');
 	}
-}	
+}
 
