41a38898329e7969c5b15d3a3c0283d8566f3c4e
Cody <codywalenciak@gmail.com>
2023-09-22 09:00:43 -0700

    x10 alphabet function

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_X10 - prints alphabet 10 times
+ *
+ * Description: uses nested for loops to iterate over the alphabet ten times
+ */
+
+void print_alphabet_X10(void)
+	{
+		int i;
+		int j;
+
+		for (i = 0 ; i < 10 ; i++)
+		{
+			for (j = 'a' ; j <= 'z' ; j++)
+			{
+				_putchar(j);
+			}
+			_putchar('\n');
+		}
+	}
