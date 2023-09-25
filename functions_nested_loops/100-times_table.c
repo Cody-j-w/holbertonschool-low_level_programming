ebd8c7e89a1aa375edf4a99f1e6e22071a7939d8
Cody <codywalenciak@gmail.com>
2023-09-25 07:32:32 -0700

    fixed a typo in the ones line print and added a break case.

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -18,48 +18,39 @@ void print_times_table(int n)
 	int j;
 
 	int sum;
-	
+
 	if (n > 15 || n < 0)
-	{
 		break;
-	}
-	else
+
+	for (i = 0 ; i <= n ; i++)
 	{
-	
-		for (i = 0 ; i <= n ; i++)
+		for (j = 0 ; j <= n ; j++)
 		{
-			for (j = 0 ; j <= n ; j++)
+			sum = i * j;
+
+			if (j > 0)
 			{
-				sum = i * j;
-
-				if (j > 0)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+				if (sum < 100)
 					_putchar(' ');
-					if (sum < 100)
-						_putchar(' ');
-					if (sum < 10)
-						_putchar(' ');
-				}
-
-				if (sum > 99)
-				{
-					_putchar(sum / 100 + '0');
-					_putchar((sum / 10) % 10 + '0');
-					_putchar(sum % 10 + '0');
-				}
-				else if (sum > 9)
-				{
-					_putchar(sum / 10 + '0');
-					_putchar(sum % 10 + '0');
-				}
-				else
-				{
-					_putchar(sum + '0');
-				}
+				if (sum < 10)
+					_putchar(' ');
 			}
-			_putchar('\n');
+
+			if (sum > 99)
+			{
+				_putchar(sum / 100 + '0');
+				_putchar((sum / 10) % 10 + '0');
+			}
+			else if (sum > 9)
+			{
+				_putchar(sum / 10 + '0');
+			}
+			_putchar(sum % 10 + '0');
+
 		}
+		_putchar('\n');
 	}
 }
 
