09141a380d912a287265f94501654a75bf34b159
Cody <codywalenciak@gmail.com>
2023-09-24 19:58:49 -0700

    less disgusting now, got rid of the recursion.

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -16,29 +16,32 @@ void print_to_98(int n)
 	}
 	else
 	{
-		while (n != 98)
+		int abs = n * ((n > 0) - (n <= 0));
+
+		while (abs != 98)
 		{
-			if (n < 0)
+			if (abs < 0)
 				_putchar('-');
 
-			if (n > 99 || n < -99)
+			if (abs > 99 || abs < -99)
 			{
-				_putchar(n / 100 + '0');
-				_putchar((n / 10) % 10 + '0');
+				_putchar(abs / 100 + '0');
+				_putchar((abs / 10) % 10 + '0');
 			}
-			else if (n > 9 || n < -9)
+			else if (abs > 9 || abs < -9)
 			{
-				_putchar(n / 10 + '0');
+				_putchar(abs / 10 + '0');
 			}
-			_putchar(n % 10 + '0');
+			_putchar(abs % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
-			if (n > 98)
-				n--;
+			if (abs > 98)
+				abs--;
 			else
-				n++;
+				abs++;
 		}
 		_putchar(9 + '0');
 		_putchar(8 + '0');
+		_putchar('\n');
 	}
 }
