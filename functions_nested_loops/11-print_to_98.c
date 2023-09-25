2c3a96560ab5b83969b0e55b57a5b3bd2082b682
Cody <codywalenciak@gmail.com>
2023-09-24 19:51:26 -0700

    this is 100% overkill, but we'll see just how bad it is.

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,81 +4,41 @@
  * print_to_98 - takes an integer and prints all integers between it and 98
  * @n: the starting integer
  *
- * Description: uses multiple loops, if statements, and a bit of recursion to
- * print to properly print to 98
+ * Description: uses loops and if statements to print from n to 98.
  */
-
-void print_large(int n);
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n == 98)
 	{
-		for ( ; n >= 98 ; n--)
-		{
-			if (n == 98)
-			{
-				print_large(n);
-			}
-			else
-			{
-				print_large(n);
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	else if (n < 98)
-	{
-		for ( ; n <= 98 ; n++)
-		{
-			if (n == 98)
-			{
-				print_large(n);
-			}
-			else
-			{
-				print_large(n);
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		_putchar(9 + '0');
+		_putchar(8 + '0');
 	}
 	else
 	{
-		print_large(n);
+		while (n != 98)
+		{
+			if (n < 0)
+				_putchar('-');
+
+			if (n > 99 || n < -99)
+			{
+				_putchar(n / 100 + '0');
+				_putchar((n / 10) % 10 + '0');
+			}
+			else if (n > 9 || n < -9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+			if (n > 98)
+				n--;
+			else
+				n++;
+		}
+		_putchar(9 + '0');
+		_putchar(8 + '0');
 	}
 }
-
-/**
- * print_large - helper function that recursively prints particularly large
- * numbers.
- * @n: integer given to print_large by print_to_98 loop.
- *
- * Description: probably absolute overkill.
- */
-
-void print_large(int n)
-{
-	int N = n;
-
-	if (N < 0)
-	{
-		_putchar('-');
-		N = N * -1;
-	}
-
-	if (N == 0)
-		_putchar('0');
-
-	if (N / 10)
-		print_large(N);
-
-	_putchar(N % 10 + '0');
-}
-
-
-
-
-
-
