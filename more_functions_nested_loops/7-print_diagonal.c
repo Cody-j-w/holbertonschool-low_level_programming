5e1e239afec8540bd575933ac63bb660dbbdc6f0
Cody <codywalenciak@gmail.com>
2023-09-27 08:35:08 -0700

    function that prints diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line with a length of n
+ *
+ * @n: the number of segments in the diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	int j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < i ; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+		_putchar('\n');
+	}
+}
