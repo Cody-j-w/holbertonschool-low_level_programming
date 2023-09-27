76f4b59559ba0c21beb4d84d9174d892adfc0413
Cody <codywalenciak@gmail.com>
2023-09-27 07:39:43 -0700

    added basic _isdigit fuction recreation

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - determines whether a given character is a digit or not
+ *
+ * @c: a single character.
+ *
+ * Return: 1 if c is a digit, 0 if c is anything else.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9');
+		return (1);
+	else
+		return (0);
+}
