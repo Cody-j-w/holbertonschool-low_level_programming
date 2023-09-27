4bed800a359aaa9dc97473b09759aed242f5f587
Cody <codywalenciak@gmail.com>
2023-09-27 08:53:20 -0700

    FIZZBUZZFIZZBUZZFIZZBUZZ

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Description: FIZZBUZZFIZZBUZZFIZZBUZZ
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		printf(" ");
+	}
+	return (0);
+}
