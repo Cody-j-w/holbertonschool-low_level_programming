35b97f7ae62774f587cb7bbfacf852f5ffae0fbc
Cody <codywalenciak@gmail.com>
2023-09-27 09:11:59 -0700

    function that prints triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of variable size
+ * 
+ * @n: the integer that determines the dimensions of the triangle
+ */
+
+void print_triangle(int n)
+{
+	int i;
+	
+	int j;
+
+	int k;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = n ; i > 0 ; i--)
+	{
+		for (j = 1 ; j < i ; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = i ; k <= n ; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
