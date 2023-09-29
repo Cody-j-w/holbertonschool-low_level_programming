b2b8420fc83b53af9045a5a63a6b68622cac2380
Cody <codywalenciak@gmail.com>
2023-09-29 09:45:57 -0700

    fixed exit condition and sign calculation

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,39 @@ int _atoi(char *str)
 {
 	int i = 0;
 
+	int neg = 0;
+
+	int pos = 0;
+
 	int sign = 1;
 
 	int result = 0;
 
 	while (str[i] != '\0')
 	{
-		if ((str[i] == '-' && sign > 0) || (str[i] == '+' && sign < 0))
-			sign = sign * -1;
+		if (str[i] == '-')
+		{
+			neg += 1
+		}
+		if (str[i] == '+')
+		{
+			pos += 1
+		}
+
+		if ((str[i] <= '0' || str[i] >= '9') && (result > 0))
+			break;
+		
 		if (str[i] >= '0' && str[i] <= '9')
 		{
 			result = (result * 10) + (str[i] - '0');
 		}
+	}
 		i++;
 	}
+	if (neg > pos)
+		sign = -1;
+	else
+		sign = 1;
 	return (result * sign);
 }
 
