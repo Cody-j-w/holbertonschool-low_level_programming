9c160364001ad922af1cbbcda12a4234f6cd0042
Cody <codywalenciak@gmail.com>
2023-10-04 11:53:50 -0700

    fixed an operand

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,18 +11,21 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	int j = 0;
+	int prev = 0;
+	int j;
 	char delimit[] = {',', ';', '.', ' ', '!', '?', '"', '(', ')',
 		'{', '}', '\t', '\n'};
 
 	while (str[i] != '\0')
 	{
-		for (; j < 13; j++)
+		for (j = 0; j < 13; j++)
 		{
-			if ((str[i - 1] == delimit[j] || i == 0) &&
+			if ((str[prev] == delimit[j] || i == 0) &&
 					(str[i] >= 'a' && str[i] <= 'z'))
 				str[i] -= ' ';
 		}
+		if (i > 0)
+			prev++;
 		i++;
 	}
 	return (str);
