51a56e2193447d7637404c7a430e6be7ef87e5d9
Cody <codywalenciak@gmail.com>
2023-10-11 07:41:44 -0700

    the beginning of recursive functions, starting with puts

---
+++ recursion/0-puts-recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - uses recursion to emulate the puts function
+ * @str: the string to be printed
+ */
+
+void _puts_recursion(char *str)
+{
+	if (*str == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*str);
+		_puts_recursion(str++);
+	}
+}
---
+++ recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER
+#define HEADER
+
+int _putchar(char);
+
+void _puts_recursion(char*);
+
+#endif
