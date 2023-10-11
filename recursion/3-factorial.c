8cca36ca6c2b2513231569e958ef213a2b7a62bd
Cody <codywalenciak@gmail.com>
2023-10-11 08:20:20 -0700

    factorial function using recursion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - calculate the factorial of a provided integer
+ * @n: the integer to be factorialized
+ * 
+ * Return: the factorial value, or -1 if initial value is less than 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
