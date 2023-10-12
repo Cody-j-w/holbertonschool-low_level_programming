fde15ba970fb756aaedc23b222c17f20a3d15676
Cody <codywalenciak@gmail.com>
2023-10-12 12:36:46 -0700

    program that multiplies two integers provided from args

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - multiply two provided integers
+ *
+ * @argc: the number of arguments - ideally no more or less than 3 here
+ * @argv: the list of arguments - ideally the program, and two integers
+ *
+ * Return: 0 on a success, 1 in case of an error
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", argv[1] * argv[2]);
+		return (0);
+	}
+}
