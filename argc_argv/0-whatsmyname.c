87f4036b7452a418b25f0748abe181a1bf002b23
Cody <codywalenciak@gmail.com>
2023-10-12 11:56:36 -0700

    program to print file name, even after changing

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - enter the program and print the name of the file
+ * @argc: the number of arguments
+ * @argv: the list of arguments
+ *
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);	
+}
