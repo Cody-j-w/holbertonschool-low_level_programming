b4a727b279c8eb6acd5d0b060098fbe4edbd533d
Cody <codywalenciak@gmail.com>
2023-10-12 12:29:55 -0700

    function that prints out the names of all arguements passed to it

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments passed to the program
+ * @argc: the number of arguments passed
+ * @argv: the list of arguments passed
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
