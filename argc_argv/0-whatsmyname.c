7e67988ca1bf3f7d91b343dde34c67a335578856
Cody <codywalenciak@gmail.com>
2023-10-12 12:04:58 -0700

    added newline to print statement

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  * Return: always 0
  */
 
-int main (__attribute__((unused)) int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%s", argv[0]);
-	return (0);	
+	printf("%s\n", argv[0]);
+	return (0);
 }
