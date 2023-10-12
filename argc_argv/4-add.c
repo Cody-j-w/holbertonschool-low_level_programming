6ac6aadc1829d5487e4088e030b0e9a01484c03e
Cody <codywalenciak@gmail.com>
2023-10-12 13:01:32 -0700

    function that takes two arguments and adds them together if they're integers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add the values of all arguements together
+ *
+ * @argc: the number of arguments passed
+ * @argv: the list of arguments passed
+ *
+ * Return: 0 if successful, 1 if an error is caught
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < argc)
+	{
+		if (*argv[i] > '9' || *argv[i] < '0')
+		{
+			printf("Error");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+			i++;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
