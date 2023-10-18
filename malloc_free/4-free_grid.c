31d9119d65177e9231be16b3949f9552d94d8e65
Cody <codywalenciak@gmail.com>
2023-10-18 11:05:43 -0700

    function to free up memory space occupied by a 2d array

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees memory allocated in a grid
+ * @grid: the grid being freed up
+ * height: the height of the grid
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
