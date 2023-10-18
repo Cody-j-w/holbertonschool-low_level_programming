6dd9f059d6339ff4cea2067f803ba57c9064a27e
Cody <codywalenciak@gmail.com>
2023-10-18 08:24:31 -0700

    function to concat two strings together in a new memory location

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include "2-strlen.c"
+
+/**
+ * str_concat - concatenate two strings together in an allocated memory
+ * space
+ * @str1: the leading string
+ * @str2: the tailing string
+ *
+ * Return: a pointer to the new string
+ */
+
+char *str_concat(char *str1, char *str2)
+{
+	int i;
+	int j;
+	char *new_str = malloc(_strlen(str1) + _strlen(str2) + 1);
+
+	if (new_str)
+	{
+
+		for (i = 0; i < _strlen(str1); i++)
+		{
+			new_str[i] = str1[i];
+		}
+		for (j = 0; j < _strlen(str2); j++)
+		{
+			new_str[i] = str2[j];
+			i++;
+		}
+
+		return (new_str);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -7,5 +7,7 @@ char *create_array(unsigned int, char);
 
 char *_strdup(char *);
 
+char *str_concat(char *, char *);
+
 
 #endif
