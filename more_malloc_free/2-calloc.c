90d85da874a2784b202a1bd2bbf246c071a21dab
Cody <codywalenciak@gmail.com>
2023-10-19 10:58:22 -0700

    experimenting with an alternate data type to avoid malloc errors

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@
  void *_calloc(unsigned int nmemb, unsigned int size)
  {
  	unsigned int i = 0;
-	void *new_arr;
+	unsigned char *new_arr;
 	unsigned int val;
 
 	if (nmemb >= 1 && size >= 1)
@@ -28,7 +28,7 @@
 	{
 		while (i < val)
 		{
-			new_arr[i] = 0;
+			new_arr[i] = '0';
 			i++;
 		}
 		return (new_arr);
