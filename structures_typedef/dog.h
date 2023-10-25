4610d0fa5b5def8098499d651e0b3f11c1a313f9
Cody <codywalenciak@gmail.com>
2023-10-25 12:14:41 -0500

    Add new typedef for the dog struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,12 @@ struct dog
 	char *owner;
 };
 
+
+/**
+ * dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *, char *, float, char *);
 
 void print_dog(struct dog *);
