dc479a0f57183e3b44655fc9b7abca5cbc49e2dc
Cody <codywalenciak@gmail.com>
2023-10-25 10:34:46 -0500

    Fix missing semicolon.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
+/**
+ * struct Dog - a simple struct containing the details for a dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+*/
+
 struct Dog
 {
     char *name;
     float age;
     char *owner;
-}
+};
