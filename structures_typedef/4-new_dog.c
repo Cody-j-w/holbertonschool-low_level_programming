c588bafb595a7a96f7ba5cae27c047353b000254
Cody <codywalenciak@gmail.com>
2023-10-25 15:05:33 -0500

    Testing some options.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,9 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (puppy == NULL)
 		return (0);
 
-	_strcpy(puppy->name, name);
+	puppy->name = name;
 	puppy->age = age;
-	_strcpy(puppy->owner, owner);
+	puppy->owner = owner;
 
 	return (puppy);
 }
