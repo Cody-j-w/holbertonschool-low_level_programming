4714d12cfae95679270949a9ed73772e9033e95c
Cody <codywalenciak@gmail.com>
2023-10-26 10:02:48 -0500

    Account for a NULL struct being passed to function

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -8,7 +8,12 @@
 
 void free_dog(dog_t *dog)
 {
-    free((*dog).name);
-    free((*dog).owner);
-    free(dog);
+	if (dog)
+	{
+		if ((*dog).name)
+			free((*dog).name);
+		if ((*dog).owner)
+			free((*dog).owner);
+		free(dog);
+	}
 }
