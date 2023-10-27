ba12d008042d4bf2f704361bd3ea9007fa03bd59
Cody <codywalenciak@gmail.com>
2023-10-27 10:57:28 -0500

    Fix comparison error in main file

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 		return (98);
-	if (argv[2] == "/" && argv[3] == "0")
+	if (argv[2][0] == '/' && atoi(argv[3]) == 0)
 		return (100);
 	
 	res = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
