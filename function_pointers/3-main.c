fe5f4702b6c159176d6a647ea35b840de22bf631
Cody <codywalenciak@gmail.com>
2023-10-27 10:54:04 -0500

    Initial commit of calculator program

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: the list of arguments passed to the program
+ * 
+ * Return: 0 on success, 98 if the incorrect number of arguments
+ * is passed, 99 if an invalid operator is used, and 100 if 
+ * any form of division by 0 is attempted
+*/
+
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc != 4)
+		return (98);
+	if (argv[2] == "/" && argv[3] == "0")
+		return (100);
+	
+	res = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+
+	if (!res)
+		return (99);
+
+	printf("%d\n", res);
+	return (0);
+}
--- function_pointers/README.md
+++ function_pointers/README.md
@@ -1,14 +1,23 @@
 # Function Pointers
+
 A collection of function pointers and their uses.
 
 ## 0-print_name
-A function that takes a string and a function, and uses that 
+
+A function that takes a string and a function, and uses that
 function to print the string.
 
 ## 1-array_iterator
+
 A function that takes an array and a function, and executes
 the given function on each element of the array.
 
 ## 2-int_index
+
 A function that takes an array and a function, and compares
 the values in that array against a value held in the function.
+
+## 3-calc
+
+A multi-file program that provides basic calculator functionality.
+Uses function pointers and structs to keep operators organized.
