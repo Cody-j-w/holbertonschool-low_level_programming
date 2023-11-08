de6455e10e59d4d5e50aa16dd7d306f8925c6a37
Cody <codywalenciak@gmail.com>
2023-11-08 14:47:33 -0600

    Add print_listint function

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - prints the integers in the list
+ * @head: the beginning of the list
+ * 
+ * Return: the number of nodes in the list
+*/
+
+size_t print_listint(const listint_t *head)
+{
+	size_t count = 0;
+
+	while (head != NULL)
+	{
+		count++;
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+	return (count);
+}
