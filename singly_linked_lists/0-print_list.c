3ea2dfb3c1bc58ffead4258f4106c6cd4a03eeb0
Cody <codywalenciak@gmail.com>
2023-11-08 09:43:11 -0600

    Quick style fixes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,22 +5,22 @@
 /**
  * print_list - prints the data of all nodes that follow @head
  * @head: the list node given as the beginning of the list to be printed
- * 
+ *
  * Return: the length of the list
 */
 
 size_t print_list(const list_t *head)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (*head != NULL)
-    {
-        count++;
-        if (head->str == NULL)
-            printf("[0] (nil)");
-        else
-            printf("[%d] %s\n", head->len, head->str);
-        head = head->next;
-    }
-    return (count);
+	while (*head != NULL)
+	{
+		count++;
+		if (head->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", head->len, head->str);
+		head = head->next;
+	}
+	return (count);
 }
