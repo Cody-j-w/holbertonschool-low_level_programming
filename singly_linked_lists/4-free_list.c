88944e2a679ba8633c473a1453dbd3bd857eb9bf
Cody <codywalenciak@gmail.com>
2023-11-08 13:44:38 -0600

    Added documentation, cleaned up style

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * free_list - frees a list from allocated memory
+ * @head: the beginning of the list to be freed
+*/
+
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        if (temp->str)
-            free(temp->str);
-        free(temp);
-    }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		if (temp->str)
+			free(temp->str);
+		free(temp);
+	}
 }
