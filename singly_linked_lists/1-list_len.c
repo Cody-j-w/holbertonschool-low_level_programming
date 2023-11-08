5b437daad1f37fc941e1b6119c875fd3ac376ac9
Cody <codywalenciak@gmail.com>
2023-11-08 10:22:44 -0600

    Added documentation to list_len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - gets the list of a provided linked list
+ * @head: the beginning of the list
+ * 
+ * Return: the length of the list that follows @head
+*/
+
 size_t list_len(const list_t *head)
 {
 	size_t count = 0;
