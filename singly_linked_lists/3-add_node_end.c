16c4c7aebc9f9453eab58ec480999bf410c5204b
Cody <codywalenciak@gmail.com>
2023-11-08 13:16:44 -0600

    Fixed NULL check

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -18,10 +18,8 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 	current = malloc(sizeof(list_t));
-	if (!new_node || !current)
-	{
+	if (!new_node)
 		return (NULL);
-	}
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
