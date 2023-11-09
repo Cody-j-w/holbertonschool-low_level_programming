70f90923fa53df27fa5ed283cd9512140919bfb8
Cody <codywalenciak@gmail.com>
2023-11-09 14:00:41 -0600

    Add pop_listint function

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - removes the head from a listint list and returns its value
+ * @head: a pointer to the head of the given list, to be popped.
+ *
+ * Return: the value held at @head.
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int value;
+
+	temp = *head;
+	value = temp->n;
+	*head = (*head)->next;
+	free(temp);
+	return (value);
+}
