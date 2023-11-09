bb2107fa44f9732958943f64db1b3763a32e0cf3
Cody <codywalenciak@gmail.com>
2023-11-09 15:14:25 -0600

    Add sum_listint function

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sums the values held in the nodes in a list
+ * @head: the head of the list
+ *
+ * Return: the sum of the values
+*/
+
+int sum_listint(listint_t *head);
+{
+	listint_t *count;
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		count = head;
+		while (count != NULL)
+		{
+			sum += count->n;
+			count = count->next;
+		}
+	}
+	return (sum);
+}
