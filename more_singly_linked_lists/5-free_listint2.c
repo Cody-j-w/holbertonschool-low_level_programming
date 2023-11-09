523c3d37259aea54463362285ba9cc4026c849a0
Cody <codywalenciak@gmail.com>
2023-11-09 10:31:04 -0600

    Add free_listint2 function

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint - frees a list from allocated memory
+ * @head: the beginning of the list to be freed
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	head = NULL;
+}
