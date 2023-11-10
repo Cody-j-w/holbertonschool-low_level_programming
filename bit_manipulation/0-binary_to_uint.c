2c140a127c78aeb70745d9993a0725518867977f
Cody <codywalenciak@gmail.com>
2023-11-10 14:10:03 -0600

    More tests

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -25,7 +25,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (i >= 0)
 	{
-		printf("iteration number: %d\n", i);
+		printf("iteration number: %d\ncurrent bit is %c with an ascii value of %d\n", i, b[i], b[i]-'0');
 		if (b[i] != 1 && b[i] != 0)
 			return (0);
 		dec += (b[i] - '0') * base;
