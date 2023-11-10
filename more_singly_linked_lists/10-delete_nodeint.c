c0c5ea4db0925870637797b839398ae3971fbb50
Cody <codywalenciak@gmail.com>
2023-11-10 10:05:47 -0600

    Added return

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -23,6 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
+		return (1);
 	}
 	prev = *head;
 	while (i < index - 1)
