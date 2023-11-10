26a03ccbb9d9a0942dae65a58866350d47f6b591
Cody <codywalenciak@gmail.com>
2023-11-10 08:50:06 -0600

    Testing different possible loop iterations

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head != NULL)
 	{
 		prev = *head;
-		while (i < idx)
+		while (i < idx - 1)
 		{
 			if (prev == NULL)
 				return (NULL);
