6b5a510ac86404558eec056ddd7200ea1800755a
Cody <codywalenciak@gmail.com>
2023-11-10 10:04:15 -0600

    Fixed typo

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 	}
 	prev = *head;
-	while (i < idx - 1)
+	while (i < index - 1)
 	{
 		if (prev == NULL)
 			return (-1);
