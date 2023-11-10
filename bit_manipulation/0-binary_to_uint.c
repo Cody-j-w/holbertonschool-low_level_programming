a882f4a1f017f7072aba9f2ffa32dae9e9d0a2fd
Cody <codywalenciak@gmail.com>
2023-11-10 14:17:04 -0600

    Reordered checking for a NULL string

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,13 +13,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
-	int i = strlen(b) - 1;
+	int i;
 	unsigned int base = 1;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
+	i = strlen(b) - 1;
 
 	while (i >= 0)
 	{
