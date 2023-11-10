8ba631d7e2792290160700a57eeb40c251730543
Cody <codywalenciak@gmail.com>
2023-11-10 09:19:28 -0600

    Added check for bad index

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -31,6 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	prev = *head;
 	while (i < idx - 1)
 	{
+		if (prev == NULL)
+			return (NULL);
 		prev = prev->next;
 		i++;
 	}
