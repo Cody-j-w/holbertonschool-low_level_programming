a02417ac79c6d4eddadda9556e68bbe2d0f70558
Cody <codywalenciak@gmail.com>
2023-11-11 15:40:03 -0600

    Added check for out of bounds error

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -7,17 +7,21 @@
  * set_bit - set a specified bit of an integer to 1
  * @n: a pointer to the integer being changed
  * @index: the specific bit of @n that is being flipped
- * 
+ *
  * Return: 1 if the change is successful, -1 if not
 */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    int mask = 1 << index;
+	int mask = 1 << index;
+	unsigned int sigbit = sizeof(unsigned long int) * 8 - 1;
 
-    *n = *n ^ mask;
+	if (index > sigbit)
+		return (-1);
 
-    if (get_bit(*n, index) == 1)
-        return (1);
-    return (-1);
+	*n = *n ^ mask;
+
+	if (get_bit(*n, index) == 1)
+		return (1);
+	return (-1);
 }
