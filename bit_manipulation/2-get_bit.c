d4727da72a165a308d81a956fc0e989273258d0a
Cody <codywalenciak@gmail.com>
2023-11-11 12:49:14 -0600

    Add get_bit function

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - gets the value of the bit that aligns with the specified index
+ * @n: the number being checked
+ * @index: the specified index
+ *
+ * Return: the value of the bit, or -1 if an error occurs
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+	int bit;
+	int sigbit = sizeof(int) * 16 - 1;
+
+	while (((n >> sigbit) & 1) == 0)
+		sigbit--;
+
+	for (i = 0; i <= sigbit; i++)
+	{
+		bit = (n >> i) & 1;
+		if (i == index)
+			return (bit);
+	}
+	return (-1);
+}
