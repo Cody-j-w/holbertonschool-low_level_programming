f4d504184d509b7c8fbd1b3a0fe491cf78b783ef
Cody <codywalenciak@gmail.com>
2023-11-13 13:46:36 -0600

    Typo fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	filedes = open(filename, O_RDWR, O_CREAT, O_TRUNC);
 
 	if (!filedes)
-		Return (-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
