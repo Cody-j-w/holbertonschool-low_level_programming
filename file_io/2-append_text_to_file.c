ec7cae59a57412d3a10887e8dc4636524a5d3079
Cody <codywalenciak@gmail.com>
2023-11-13 16:20:28 -0600

    Style fixes

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,27 +10,27 @@
  * append_text_to_file - appends a string to the end of a provided file
  * @filename: the name of the file to be appended to
  * @text_content: the string being appended
- * 
+ *
  * Return: 1 on success, -1 on error
 */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int filedes;
-    unsigned long int i;
+	int filedes;
+	unsigned long int i;
 
-    if (filename == NULL)
-        return (-1);
-    filedes = open(filename, O_RDWR | O_APPEND);
-    if (filedes == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	filedes = open(filename, O_RDWR | O_APPEND);
+	if (filedes == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; i < strlen(text_content); i++)
-        {
-            write(filedes, &text_content[i], 1);
-        }
-    }
-    return (1);
+	if (text_content != NULL)
+	{
+		for (i = 0; i < strlen(text_content); i++)
+		{
+			write(filedes, &text_content[i], 1);
+		}
+	}
+	return (1);
 }
