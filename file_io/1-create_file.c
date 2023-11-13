9bdaa5857ee5a5959df244af6e17d1663c29ab73
Cody <codywalenciak@gmail.com>
2023-11-13 13:52:03 -0600

    Change order of oflags

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	filedes = open(filename, O_RDWR, O_CREAT, O_TRUNC);
+	filedes = open(filename, O_CREAT, O_RDWR, O_TRUNC);
 
 	if (!filedes)
 		return (-1);
