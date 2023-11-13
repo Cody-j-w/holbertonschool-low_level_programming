faf5718baa6b40260cdd8ba6257444d674acaae1
Cody <codywalenciak@gmail.com>
2023-11-13 14:19:59 -0600

    Add user permissions code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,14 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	filedes = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	filedes = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (!filedes)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (i = 0; i <= strlen(text_content); i++)
+		for (i = 0; i < strlen(text_content); i++)
 		{
 			write(filedes, &text_content[i], 1);
 		}
