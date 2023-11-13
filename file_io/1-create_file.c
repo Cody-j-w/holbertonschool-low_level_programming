0fb435e08645955aed93c7530a44e9da03bb52df
Cody <codywalenciak@gmail.com>
2023-11-13 14:26:38 -0600

    Test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,8 +24,7 @@ int create_file(const char *filename, char *text_content)
 
 	filedes = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (!filedes)
-		return (-1);
+	printf("file descriptor: %d\n", filedes);
 
 	if (text_content != NULL)
 	{
