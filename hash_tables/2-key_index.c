1ba036d8472f6dc76eda52241d8ea2e3ef859388
Cody <codywalenciak@gmail.com>
2023-11-16 13:09:19 -0600

    Style fixes

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,12 +6,12 @@
  * key_index - get the hash table index of a key
  * @key: the key being indexed
  * @size: the size of the hash table
- * 
+ *
  * Return: the index of the key
 */
 
 unsigned long int key_index(unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash = hash_djb2(key);
-    return (hash % size);
+	unsigned long int hash = hash_djb2(key);
+	return (hash % size);
 }
