6af00e850233f68011b1dd925a7c04d7fc398ff0
Cody <codywalenciak@gmail.com>
2023-11-16 13:10:49 -0600

    Fix param typo

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
  * Return: the index of the key
 */
 
-unsigned long int key_index(unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash = hash_djb2(key);
 	return (hash % size);
