72d29d36a37dc8069b488c57a27308c1fb60eb6e
Cody <codywalenciak@gmail.com>
2023-11-17 13:48:15 -0600

    Fix variable declarations

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,27 +10,30 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int count = i = j = 0;
-    hash_node_t *temp = NULL;
+	unsigned long int count = 0;
+	unsigned long int i = 0;
+	unsigned long int j = 0;
+	hash_node_t *temp = NULL;
 
-    if (ht)
-    {
-        printf("{");
-        while (i < ht->size)
-        {
-            if (ht->array[i] != NULL)
-            {
-                temp = ht->array[i];
-                while (temp != NULL)
-                {
-                    if (count > 0)
-                        printf(", ");
-                    printf("'%s': '%s'", temp->key, temp->value);
-                    temp = temp->next;
-                }
-            }
-            i++;
-        }
-        printf("}");
-    }
+	if (ht)
+	{
+		printf("{");
+		while (i < ht->size)
+		{
+			if (ht->array[i] != NULL)
+			{
+				temp = ht->array[i];
+				while (temp != NULL)
+				{
+					if (count > 0)
+						printf(", ");
+					printf("'%s': '%s'", temp->key, temp->value);
+					temp = temp->next;
+					count++;
+				}
+			}
+			i++;
+		}
+		printf("}");
+	}
 }
