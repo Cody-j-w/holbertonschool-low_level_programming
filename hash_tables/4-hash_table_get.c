3501e60cfff84e40b252139591e67c9c5f9e811c
Cody <codywalenciak@gmail.com>
2023-11-17 12:04:29 -0600

    Fix printf format specifier

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	printf("At index: %d\n", index);
+	printf("At index: %lu\n", index);
 	while (temp != NULL)
 	{
 		printf("Current node's value is: %s\n", temp->value);
