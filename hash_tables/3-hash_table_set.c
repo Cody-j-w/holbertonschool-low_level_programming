157586f35c847f84a0694e87fb979875b23288f7
Cody <codywalenciak@gmail.com>
2023-11-17 14:30:37 -0600

    Back to square one temporarily

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,11 +31,7 @@ int hash_table_set(hash_table_t *h, const char *key, const char *value)
 
 	if (h->array[index] == NULL || strcmp(key, h->array[index]->key) == 0)
 	{
-		temp = h->array[index];
 		h->array[index] = new_node;
-		free(temp->key);
-		free(temp->value);
-		free(temp);
 		return (1);
 	}
 	temp = h->array[index];
