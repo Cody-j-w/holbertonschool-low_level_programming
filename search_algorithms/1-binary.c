aafdd95a6477a7898926072965c0620bdbc88399
Cody <codywalenciak@gmail.com>
2023-12-18 17:05:16 -0600

    Adjust safety case

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *arr, size_t size, int value)
 
 	while (left <= right)
 	{
-		if (left + 1 == right && arr[left + 1] != value)
+		if (left == right && arr[left] != value)
 			return (-1);
 		index = left + (right - left) / 2;
 		printf("Searching in array: ");
